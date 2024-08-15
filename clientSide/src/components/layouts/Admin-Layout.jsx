b013ba8f482a07d0d45f83fbcbb953495d85b4ce
karamat Ali <maqpoon4@gmail.com>
2024-08-16 00:44:39 +0500

    admin design changes

--- clientSide/src/components/layouts/Admin-Layout.jsx
+++ clientSide/src/components/layouts/Admin-Layout.jsx
@@ -5,16 +5,16 @@ function AdminLayout() {
   return (
     <>
     
-    <h1>Admin Dashboard</h1>
-    <div className="container-fluid">
+    <div className="container-fluid mt-5 text-center" style={{height:"100vh",width:"100%"}}>
+    <h1 style={{fontSize:"4rem",fontWeight:"bold"}}>Admin Dashboard</h1>
                       
-                    <div className="row" >
+                    <div className="row d-flex justify-content-center align-items-center" >
 
-                    <div className="col col-12 col-md-12" >
+                    <div className="col col-12 col-md-11 text-center" >
     <Adminmenu/>
 
                     </div>
-                    
+                   
                 </div>
 
                    </div> 
--- clientSide/src/components/layouts/Admin-menu.jsx
+++ clientSide/src/components/layouts/Admin-menu.jsx
@@ -5,6 +5,7 @@ import { FaUser } from "react-icons/fa";
 import { RiContactsBook3Fill } from "react-icons/ri";
 import { GrBusinessService } from "react-icons/gr";
 import { useAuth } from '../../store/auth';
+import { IoMdLogOut } from "react-icons/io";
 
 
 function Adminmenu() {
@@ -20,12 +21,17 @@ function Adminmenu() {
   return (
     <>
     <header>
-      <nav>
+      <nav className='d-flex justify-content-between align-items-center'>
         <ul>
           <li> <NavLink to="/"> <IoHomeSharp/> Home </NavLink> </li>
           <li><NavLink to="/admin/users"> <FaUser/> Users </NavLink> </li>
           <li><NavLink to="/admin/contacts"> <RiContactsBook3Fill/> Contact </NavLink> </li>
           <li><NavLink to="/service"> <GrBusinessService/> Services </NavLink> </li>
+          
+        </ul>
+        <ul>
+          <li> <NavLink to="/logout"> <IoMdLogOut/> Logout </NavLink> </li>
+          
           
         </ul>
       </nav>
